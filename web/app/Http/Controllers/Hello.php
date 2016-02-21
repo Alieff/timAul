b9ab2e3f04d4a87691ac6b05b174f24c82361adb
serdralfs <serdralfs@gmail.com>
2016-02-21 08:04:14 +0700

    Hello worldable

--- web/app/Http/Controllers/Hello.php
+++ web/app/Http/Controllers/Hello.php
@@ -9,5 +9,17 @@ use App\Http\Controllers\Controller;
 
 class Hello extends Controller
 {
+	public function index()
+	{
+	    return 'hello world from controller : )';
+	}
+
+	public function show($name)
+	{
+		// die($name);
+	    return view('hello',array('name' => $name));
+	}
+
+
     //
 }
--- web/app/Http/routes.php
+++ web/app/Http/routes.php
@@ -12,8 +12,20 @@
 */
 
 Route::get('/', function () {
+	echo 'welcome';
     return view('welcome');
 });
+
+Route::get('/hello/{name}', 'Hello@show');
+
+// Route::get('/hello',function(){
+//     return 'Hello World!';
+// });
+
+
+Route::get('hello', 'Hello@index');
+
+
 
 /*
 |--------------------------------------------------------------------------
