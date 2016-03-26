0aad3dd7b2eb57d2d796cf6ab7261e9ece85b750
pflarasati <putif9l@gmail.com>
2016-03-26 20:11:47 +0700

    Add detailed comments to class InspireCrawler and LogCrawl

--- InspireCrawler/src/InspireCrawler.java
+++ InspireCrawler/src/InspireCrawler.java
@@ -18,8 +18,7 @@ public class InspireCrawler extends WebCrawler {
 
     /**
      * FILTER untuk mencegah crawler masuk ke alamat url yang mempunyai extensi yang disebut dibawah
-     * Dibawah ini contoh
-     * Konten website yang tidak mungkin di kunjungi
+     * Dibawah ini contoh konten website yang tidak mungkin di kunjungi
      */
     private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg"
             + "|png|mp3|mp4|zip|gz|pdf))$");
@@ -31,7 +30,7 @@ public class InspireCrawler extends WebCrawler {
      private QuoteFilter quoteFilter;
 
     /**
-     * Constructor
+     * Constructor dari class InspireCrawler
      */
     public InspireCrawler (){
         this.quoteFilter =  CrawlerController.quoteFilter;
@@ -40,17 +39,15 @@ public class InspireCrawler extends WebCrawler {
         this.configReader = new ConfigReader();
     }
     /**
-     * Disini adalah aturan dari crawler untuk dapat mengakses web yang akan dijelajahi.
-     * Baca config dengan menginisialisasi class config yang dibuat EGA
-     * Buat batasan konten yang crawler bisa baca untuk Pattern FILTERS
+     * Method shouldVisit
+     * Method ini berisi batasan apakah crawler boleh meng-crawl sebuah website atau tidak
+     * Method ini bertujuan untuk mengecek apakah halaman web yang akan di-crawl valid atau tidak
      * @param referringPage page halaman yang merefer ke url
      * @param url page halaman yang akan dikunjungi
      * @return apakah boleh menuju halaman
      */
     @Override
     public boolean shouldVisit(Page referringPage, WebURL url) {
-
-
 
         //arraylist daftar web yang boleh di crawl
         ArrayList<String> daftarWeb = configReader.getWebAddress();
@@ -61,7 +58,7 @@ public class InspireCrawler extends WebCrawler {
         //flag
         boolean yesVisit = false;
 
-        //cek apakah web yang akan dikunjungi ada di list daftar web
+        //cek apakah web yang akan dikunjungi ada di list daftar web dan merupakan halaman yang valid
         for(int i=0; i<daftarWeb.size(); i++) {
             yesVisit = !FILTERS.matcher(href).matches() && href.startsWith(daftarWeb.get(i));
 
