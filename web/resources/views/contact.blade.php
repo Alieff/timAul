2fe44abf3a2733235d11a58250080a40663aef99
pflarasati <putif9l@gmail.com>
2016-04-19 17:37:57 +0700

    Update contact and faq page with Laravel integration

---
+++ web/resources/views/contact.blade.php
@@ -0,0 +1,3 @@
+@extends('layouts.page')
+@section('bodycontent')
+<img style="width:100%;padding-top:50px;" src="..\resources\assets\images\landing.jpg" class="img-responsive" alt="Responsive image">
--- web/resources/views/faq.blade.php
+++ web/resources/views/faq.blade.php
@@ -1,46 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Contact</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="../assets/css/templateInspire.css">
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-</head>
-<body>
-<nav class="navbar navbar-fixed-top" role="navigation">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbarCollapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a id="logo" class="navbar-brand" href="#">Inspire Crawler</a></a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul id="nav-text" class="nav navbar-nav navbar-right">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">API</a></li>
-                <li class="active"><a href="#">Contact</a></li>
-                <li><a href="#">FAQ</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-<!-- Image Header-->
-<img style="width:100%;padding-top:50px;" src="../assets/img/landing.jpg" class="img-responsive" alt="Responsive image">
-
+@extends('layouts.page')
+@section('bodycontent')
+<img style="width:100%;padding-top:50px;" src="..\resources\assets\images\landing.jpg" class="img-responsive" alt="Responsive image">
 <!-- Question Answer -->
 <div class="container" id='faq-wrapper'>
     <div class="row">
@@ -130,20 +90,4 @@
           </div>
         </div>
     </div>
-
-<footer class="footer">
-        <div class="container">
-        <span style="color: #fff" id="copyright" style="height: 100%;line-height: 100%;">
-        &copy; Copyright - TimAul 2016
-        </span>
-        <span style="float: right">
-            <a href="#"><i class="fa fa-facebook"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
-            <a href="#"><i class="fa fa-linkedin"></i></a>
-            <a href="#"><i class="fa fa-google-plus"></i></a>
-            <a href="#"><i class="fa fa-skype"></i></a>
-        </span><!--End container-->
-        </div>
-</footer><!--End footer 2-->
-</body>
-</html>
+@endsection
