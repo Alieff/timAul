339bdf96a3bd53dcfa067277515f2d67f178ba70
pflarasati <putif9l@gmail.com>
2016-04-24 12:52:27 +0700

    Finalize and add QA for FAQ page

--- web/resources/views/faq.blade.php
+++ web/resources/views/faq.blade.php
@@ -1,93 +1,94 @@
 @extends('layouts.page')
 @section('bodycontent')
-<img style="width:100%;padding-top:50px;" src="..\resources\assets\images\landing.jpg" class="img-responsive" alt="Responsive image">
-<!-- Question Answer -->
-<div class="container" id='faq-wrapper'>
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <!-- Tab panes -->
-            <div class="tab-content faq-cat-content">
-                <div class="tab-pane active in fade" id="faq-cat-1">
-                    <div class="panel-group" id="accordion-cat-1">
-                        <div class="panel panel-default panel-faq">
-                            <div class="panel-heading">
-                                <a data-toggle="collapse" data-parent="#accordion-cat-1" href="#faq-cat-1-sub-1">
-                                    <h4 class="panel-title">
-                                        Question 1 - Lorem ipsum dolor sit amet
-                                        <span class="pull-right"><i class="glyphicon glyphicon-plus"></i></span>
-                                    </h4>
-                                </a>
-                            </div>
-                            <div id="faq-cat-1-sub-1" class="panel-collapse collapse">
-                                <div class="panel-body">
-                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia gravida vestibulum. Cras scelerisque sodales aliquam. Morbi ultrices molestie libero at rutrum. Vivamus a aliquet purus. In fringilla metus a sodales blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc congue pellentesque erat vel facilisis. Nulla convallis sapien id posuere ultricies. Nam luctus mi ac vulputate consequat.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default panel-faq">
-                            <div class="panel-heading">
-                                <a data-toggle="collapse" data-parent="#accordion-cat-1" href="#faq-cat-1-sub-1">
-                                    <h4 class="panel-title">
-                                        Question 2 - Lorem ipsum dolor sit amet
-                                        <span class="pull-right"><i class="glyphicon glyphicon-plus"></i></span>
-                                    </h4>
-                                </a>
-                            </div>
-                            <div id="faq-cat-1-sub-1" class="panel-collapse collapse">
-                                <div class="panel-body">
-                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia gravida vestibulum. Cras scelerisque sodales aliquam. Morbi ultrices molestie libero at rutrum. Vivamus a aliquet purus. In fringilla metus a sodales blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc congue pellentesque erat vel facilisis. Nulla convallis sapien id posuere ultricies. Nam luctus mi ac vulputate consequat.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default panel-faq">
-                            <div class="panel-heading">
-                                <a data-toggle="collapse" data-parent="#accordion-cat-1" href="#faq-cat-1-sub-1">
-                                    <h4 class="panel-title">
-                                        Question 3 - Lorem ipsum dolor sit amet
-                                        <span class="pull-right"><i class="glyphicon glyphicon-plus"></i></span>
-                                    </h4>
-                                </a>
-                            </div>
-                            <div id="faq-cat-1-sub-1" class="panel-collapse collapse">
-                                <div class="panel-body">
-                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia gravida vestibulum. Cras scelerisque sodales aliquam. Morbi ultrices molestie libero at rutrum. Vivamus a aliquet purus. In fringilla metus a sodales blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc congue pellentesque erat vel facilisis. Nulla convallis sapien id posuere ultricies. Nam luctus mi ac vulputate consequat.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default panel-faq">
-                            <div class="panel-heading">
-                                <a data-toggle="collapse" data-parent="#accordion-cat-1" href="#faq-cat-1-sub-1">
-                                    <h4 class="panel-title">
-                                        Question 4 - Lorem ipsum dolor sit amet
-                                        <span class="pull-right"><i class="glyphicon glyphicon-plus"></i></span>
-                                    </h4>
-                                </a>
-                            </div>
-                            <div id="faq-cat-1-sub-1" class="panel-collapse collapse">
-                                <div class="panel-body">
-                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia gravida vestibulum. Cras scelerisque sodales aliquam. Morbi ultrices molestie libero at rutrum. Vivamus a aliquet purus. In fringilla metus a sodales blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc congue pellentesque erat vel facilisis. Nulla convallis sapien id posuere ultricies. Nam luctus mi ac vulputate consequat.
-                                </div>
-                            </div>
-                        </div>
-                        <div class="panel panel-default panel-faq">
-                            <div class="panel-heading">
-                                <a data-toggle="collapse" data-parent="#accordion-cat-1" href="#faq-cat-1-sub-1">
-                                    <h4 class="panel-title">
-                                        Question 5 - Lorem ipsum dolor sit amet
-                                        <span class="pull-right"><i class="glyphicon glyphicon-plus"></i></span>
-                                    </h4>
-                                </a>
-                            </div>
-                            <div id="faq-cat-1-sub-1" class="panel-collapse collapse">
-                                <div class="panel-body">
-                                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacinia gravida vestibulum. Cras scelerisque sodales aliquam. Morbi ultrices molestie libero at rutrum. Vivamus a aliquet purus. In fringilla metus a sodales blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc congue pellentesque erat vel facilisis. Nulla convallis sapien id posuere ultricies. Nam luctus mi ac vulputate consequat.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-          </div>
+<div>
+  <img style="width:100%;padding-top:50px;" src="..\resources\assets\images\landing.jpg" class="img-responsive" alt="Responsive image">
+  <h1>Frequently Asked Question</h1>
+</div>
+
+
+<!-- FAQ -->
+<div class="container">
+  <div class="page-header ">
+    <h1 class="text-center"><small> You have questions, we have answers.</small></h1>
+  </div>
+  <br>
+
+  <div class="panel-group" id="accordion">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
+            Apakah saya dapat menggunakan API ini?
+          </a>
+        </h4>
+      </div>
+      <div id="collapseFour" class="panel-collapse collapse ">
+        <div class="panel-body">
+          Ya, bisa. Untuk saat ini InspireCrawler dapat digunakan oleh semua orang yang memiliki akses ke internet.
+          Anda juga tidak perlu membuat akun terlebih dahulu untuk menggunakan API. Namun, Anda perlu membaca term of use dari API ini terlebih dahulu.
         </div>
+      </div>
     </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapseFive">
+            Darimanakah quotes-quotes yang di-generate oleh API ini?
+          </a>
+        </h4>
+      </div>
+      <div id="collapseFive" class="panel-collapse collapse">
+        <div class="panel-body">
+          Kami mengambil berbagai macam quotes yang berasal dari beberapa websites quotes terpercaya, seperti brainyquotes.
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapseSix">
+            Apakah saya dapat menambahkan quotes ke dalam API?
+          </a>
+        </h4>
+      </div>
+      <div id="collapseSix" class="panel-collapse collapse">
+        <div class="panel-body">
+          Untuk saat ini, pengguna API biasa belum dapat menambahkan quotes secara manual.
+        </div>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapseSeven">
+            Apakah saya dapat melihat contoh penggunaan API ini?
+          </a>
+        </h4>
+      </div>
+      <div id="collapseSeven" class="panel-collapse collapse">
+        <div class="panel-body">
+          Anda dapat melihat contoh dan dokumentasi API di halaman <a href="faq.blade.php">API Overview</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapseHeight">
+              Masih ada pertanyaan lain?
+          </a>
+        </h4>
+      </div>
+      <div id="collapseHeight" class="panel-collapse collapse">
+        <div class="panel-body">
+          Anda dapat menghubungi tim developer dengan mengisi form yang ada di halaman <a href='contact.blade.php'>contact</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+        <br />
+
+
 @endsection
