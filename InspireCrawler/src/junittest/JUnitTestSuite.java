228803e648f437eae56f3f10ae2b553449cbc295
serdralfs <serdralfs@gmail.com>
2016-05-15 23:35:44 +0700

    move to var

--- InspireCrawler/src/junittest/JUnitTestSuite.java
+++ InspireCrawler/src/junittest/JUnitTestSuite.java
@@ -8,7 +8,7 @@ import org.junit.runners.Suite;
     @RunWith(Suite.class)
     @Suite.SuiteClasses({
             QuoteFilterTest.class,
-            TreeNodeTest.class
+//            TreeNodeTest.class
     })
     public class JUnitTestSuite {
 
