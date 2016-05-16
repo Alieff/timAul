c39f3dd0d5e77034b28d7ae546a9bab786c5176b
aulch <auliachair@gmail.com>
2016-05-16 23:32:53 +0700

    adding some new files for template admin

---
+++ web/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin Inspire Crawler</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link href="../resources/assets/css/admin.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="../../assets/bower_components/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <style type="text/css">
+        .center{
+            text-align: center;
+        }
+
+        .log{
+            color: white;
+            background-color: grey;
+            overflow-y: scroll; 
+            height: 250px;
+        }
+
+        .crawler{
+            color: white;
+            background-color: grey;
+        }
+    </style>
+    
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Inspire Crawler</a>
+            </div>
+            <!-- /.navbar-header -->
+
+            <ul class="nav navbar-top-links navbar-right">
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li>
+                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Dashboard</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-bar-chart-o fa-fw"></i> Area Chart Example
+                            <div class="pull-right">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown">
+                                        Actions
+                                        <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu pull-right" role="menu">
+                                        <li><a href="#">Action</a>
+                                        </li>
+                                        <li><a href="#">Another action</a>
+                                        </li>
+                                        <li><a href="#">Something else here</a>
+                                        </li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Separated link</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div id="morris-area-chart"></div>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                </div>
+                <!-- /.col-lg-8 -->
+            </div>
+            <!-- /.row -->
+
+                        <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <i class="fa fa-cogs fa-fw"></i> Crawler Controller
+                            <div class="pull-right">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-default btn-xs">
+                                        Settings
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-lg-12 center">
+                                    <button><i class="fa fa-play-circle fa-fw fa-4x"></i></button>
+                                    <button href="#"><i class="fa fa-stop-circle fa-fw fa-4x"></i></button>
+                                </div>
+                            </div>
+                            <br/>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="panel panel-default">
+                                        <div class="panel-body crawler">
+                                            crawling is running....
+                                        </div>
+                                    </div>
+                                    <br>
+
+                                    <span class="center">Crawler's Log</span>
+                                    <div class="panel panel-default">
+                                        <div class="panel-body log">
+                                            crawling....
+                                            <br>
+                                            Quote : "If there's a will there's a way"
+                                            Author : John Doe
+                                            Source : www.getfreequotes.com
+                                            <br>
+                                            Quote : "If there's a will there's a way"
+                                            Author : John Doe
+                                            Source : www.getfreequotes.com
+                                            <br>
+                                            Quote : "If there's a will there's a way"
+                                            Author : John Doe
+                                            Source : www.getfreequotes.com
+                                            <br>
+                                            Quote : "If there's a will there's a way"
+                                            Author : John Doe
+                                            Source : www.getfreequotes.com
+                                            <br>
+                                            Quote : "If there's a will there's a way"
+                                            Author : John Doe
+                                            Source : www.getfreequotes.com
+                                            <br>
+                                            Quote : "If there's a will there's a way"
+                                            Author : John Doe
+                                            Source : www.getfreequotes.com
+                                            <br>
+                                            Quote : "If there's a will there's a way"
+                                            Author : John Doe
+                                            Source : www.getfreequotes.com
+                                            <br>
+                                            Quote : "If there's a will there's a way"
+                                            Author : John Doe
+                                            Source : www.getfreequotes.com
+                                            <br>
+                                            Quote : "If there's a will there's a way"
+                                            Author : John Doe
+                                            Source : www.getfreequotes.com
+                                            <br>
+                                            Quote : "If there's a will there's a way"
+                                            Author : John Doe
+                                            Source : www.getfreequotes.com
+                                            <br>
+                                            Quote : "If there's a will there's a way"
+                                            Author : John Doe
+                                            Source : www.getfreequotes.com
+                                            <br>
+                                            Quote : "If there's a will there's a way"
+                                            Author : John Doe
+                                            Source : www.getfreequotes.com
+                                        </div>
+                                        <!-- /.panel-body -->
+                                    </div>
+                                </div>
+
+                            </div>
+                            
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                </div>
+                <!-- /.col-lg-4 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="../../assets/bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../../assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../../assets/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="../../assets/bower_components/raphael/raphael-min.js"></script>
+    <script src="../../assets/bower_components/morrisjs/morris.min.js"></script>
+    <script src="../../assets/js/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../../assets/js/sb-admin-2.js"></script>
+</body>
+</html>
