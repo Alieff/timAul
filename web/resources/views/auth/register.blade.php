74fa0c66e34554a7dd525e580188b2eba61a3fbd
kevinega <kevin.ega@ui.ac.id>
2016-05-19 22:50:21 +0700

    Admin now can add quote manually to the database using the addQuote page (Role Management for the page hasn't been implemented yet)

--- web/resources/views/auth/register.blade.php
+++ web/resources/views/auth/register.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.adminpage')
 
-@section('content')
+@section('bodycontent')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Register</div>
                 <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('register') }}">
                         {!! csrf_field() !!}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
