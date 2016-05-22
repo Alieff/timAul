bfe3a42f4de59207de07a0ca144b92a931af8656
pflarasati <putif9l@gmail.com>
2016-05-22 20:52:40 +0700

    Add more testing for User Web and Admin Web

--- web/tests/ExampleTest.php
+++ web/tests/ExampleTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
 
     // HOMEPAGE
     public function testDisplayHome()
@@ -76,7 +77,7 @@ class ExampleTest extends TestCase
              ->see('Ask us Anything');
     }
 
-    public function testContactForm()
+    public function testContactFormSuccess()
     {
         $this->visit('contact')
              ->type('Taylor', 'name')
@@ -87,12 +88,154 @@ class ExampleTest extends TestCase
              ->see('Thanks for contacting us!');
     }
 
+    public function testContactFormJustName()
+    {
+        $this->visit('contact')
+             ->type('Taylor', 'name')
+             ->press('Submit')
+             ->seePageIs('contact')
+             ->see('The email field is required.')
+             ->see('The message field is required.')
+             ->dontSee('The name field is required');
+    }
+
+    public function testContactFormJustEmail()
+    {
+        $this->visit('contact')
+             ->type('Taylor@gmail.com', 'email')
+             ->press('Submit')
+             ->seePageIs('contact')
+             ->see('The name field is required.')
+             ->see('The message field is required.')
+             ->dontSee('The email field is required.');
+    }
+    
+    public function testContactFormNotValidEmail()
+    {
+        $this->visit('contact')
+             ->type('Taylor', 'email')
+             ->press('Submit')
+             ->seePageIs('contact')
+             ->see('The email must be a valid email address.')
+             ->see('The name field is required.')
+             ->see('The message field is required.');
+    }
+
+    public function testContactFormJustMessage()
+    {
+        $this->visit('contact')
+             ->type('Hello there', 'message')
+             ->press('Submit')
+             ->seePageIs('contact')
+             ->see('The name field is required.')
+             ->see('The email field is required.')
+             ->dontSee('The message field is required.');
+    }
+
+    public function testContactFormNameEmail()
+    {
+        $this->visit('contact')
+             ->type('James', 'name')
+             ->type('James@gmail.com', 'email')
+             ->press('Submit')
+             ->seePageIs('contact')
+             ->dontSee('The name field is required.')
+             ->dontSee('The email field is required.')
+             ->see('The message field is required.');
+    }
+
+    public function testContactFormNameMessage()
+    {
+        $this->visit('contact')
+             ->type('James', 'name')
+             ->type('Hello there, i have question', 'message')
+             ->press('Submit')
+             ->seePageIs('contact')
+             ->dontSee('The name field is required.')
+             ->see('The email field is required.')
+             ->dontSee('The message field is required.');
+    }
+
+    public function testContactFormEmailMessage()
+    {
+        $this->visit('contact')
+             ->type('James@gmail.com', 'email')
+             ->type('Hello there, i have question', 'message')
+             ->press('Submit')
+             ->seePageIs('contact')
+             ->see('The name field is required.')
+             ->dontSee('The email field is required.')
+             ->dontSee('The message field is required.');
+    }
 
     // FAQ
     public function testDisplayFAQ()
     {
         $this->visit('faq')
              ->see('You have Questions');
-    }    
+    }   
+
+    //ADMIN DASHBOARD
+    public function testUnauthenticatedUserDashboardAccess() {
+        $response = $this->call('GET', 'admin/dashboard');
+        $this->assertRedirectedTo('login');
+    }
+
+    public function testAuthenticateUserDashboardAccess()
+    {
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('admin/dashboard')
+             ->see($user->name);
+    }
+
+    //ADMIN CRUD 
+    public function testUnauthenticatedUserCRUDAccess() {
+        $response = $this->call('GET', 'admin/crud');
+        $this->assertRedirectedTo('login');
+    }
+
+    public function testAuthenticateUserCRUDAccess()
+    {
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('admin/crud')
+             ->see($user->name)
+             ->see('CRUD');
+    }
+
+    //ADMIN SEARCH
+    public function testUnauthenticatedUserSearchAccess() {
+        $response = $this->call('GET', 'admin/search');
+        $this->assertRedirectedTo('login');
+    }
+
+    public function testAuthenticateUserSearchAccess()
+    {
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('admin/search')
+             ->see($user->name)
+             ->see('Find Quote');
+    }
+
+    //ADMIN CRAWLER SETTING
+    public function testUnauthenticatedUserCrawlerSettingAccess() {
+        $response = $this->call('GET', 'admin/setting');
+        $this->assertRedirectedTo('login');
+    }
+
+    public function testAuthenticateUserCrawlerSettingAccess()
+    {
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->visit('admin/setting')
+             ->see($user->name)
+             ->see('Settings Crawler');
+    }
 
 }
